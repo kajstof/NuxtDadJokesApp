7863f677325a77407226bbf2ebca8d2e07c55ab2
Krzysztof Krysiak <krysiak86@gmail.com>
2019-06-12 13:37:57 +0200

    Add displaying jokes

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <div>jokes</div>
+  <div>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
+  </div>
 </template>
 
 <script>
-export default { }
+import Joke from '../../components/Joke';
+export default {
+  components: {
+    Joke
+  },
+  async asyncData({ $axios }) {
+    const ip = await $axios.$get('http://icanhazip.com');
+    const config = { headers: { 'Accept': 'application/json' } };
+    let res = [];
+    try {
+      res = await $axios.$get('https://icanhazdadjoke.com/search', config);
+    } catch (err) {
+      console.log(err);
+    }
+    return {
+      jokes: res.results
+    }
+  },
+  head() {
+    return {
+      title: 'Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
